8fff5288bff96c8c567fbd193b59f6b4355ef64b
designyamah <nnyamahjoseph@gmail.com>
2022-10-03 15:30:35 +0100

    nav bar js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+function openMenu(){
+    menu.style.cssText =  'visibility: visible; opacity: 1;'
+}
+
+function closeMenu(){
+    menu.style.cssText = 'visibility: hidden; opacity: 0;'
+}
